6517e982c08771bfe12fb6f76d1fed7d5aee9571
sss22213 <n0404.n0404@gmail.com>
2020-06-28 13:58:38 +0000

    dev

--- src/gpio.c
+++ src/gpio.c
@@ -1,6 +1,41 @@
 #define BCM2711
 #include "gpio.h"
+//
+int mem_fd;
+unsigned *gpio_map;
+// I/O access
+volatile unsigned *gpio;
+//
+void gpio_setup()
+{
+    // open memory
+    if( (mem_fd = open("/dev/mem", O_RDWR|O_SYNC)) < 0)
+    {
+        printf("can't not open /dev/mem");
+        exit(-1);
+    }
+    // mmap
+    gpio_map = mmap(
+        NULL,
+        BLOCK_SIZE,
+        PROT_READ|PROT_WRITE,
+        MAP_SHARED,
+        mem_fd,
+        PERI_BASE
+    );
+    // close 
+    close(mem_fd);
+    //
+    if (gpio_map == MAP_FAILED)
+    {
+        printf("mmap error");
+        exit(-2);
+    }
+    gpio = (volatile unsigned *)gpio_map;
+}
+
 int main()
 {
+    gpio_setup();
     return 0;
 }
--- src/gpio.h
+++ src/gpio.h
@@ -1,5 +1,10 @@
 #ifndef __goio__h__
 #define __goio__h__
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 #include <stdint.h>
 
 // pi2,pi3
@@ -9,21 +14,29 @@
 #else
     #define PERI_BASE 0xFE000000
 #endif
+
 // GPIO base address
 #define GPIO_BASE (PERI_BASE + 0x200000)
 
-typedef struct gpio gpio;
+// Page
+#define PAGE_SIZE (4*1024)
+#define BLOCK_SIZE (4*1024)
+
+//typedef struct gpio gpio;
 typedef enum
 {
     out = 0,
     in = 1
 }gpio_func;
 
-typedef struct gpio 
+/*
+typedef struct gpio_config
 {
     uint8_t gpio_pin; // pin name
     uint8_t gpio_func; // out or in
 }gpio;
-
+*/
+// gpio setup (mmap setup)
+void gpio_setup();
 
 #endif
